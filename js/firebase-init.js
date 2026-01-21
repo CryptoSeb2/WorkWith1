dbe442bca0f03beb85dc5a974263d751cf178fa0
CryptoSeb2 <senplays167@gmail.com>
2026-01-21 14:41:08 +0100

    Initialize Firebase app with authentication and storage

---
+++ js/firebase-init.js
@@ -0,0 +1,15 @@
+// js/firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js";
+
+const firebaseConfig = {
+  // <-- PASTE YOUR firebaseConfig HERE
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
