058518455289eeb2c0226b279203327760036ef0
CryptoSeb2 <senplays167@gmail.com>
2026-01-21 15:28:34 +0100

    Update profile.js

--- js/js/profile.js
+++ js/js/profile.js
@@ -1,14 +1,1 @@
-import { db, auth } from "./firebase-init.js";
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-export async function saveProfile(data) {
-  const user = auth.currentUser;
-  if (!user) return;
-
-  await setDoc(doc(db, "users", user.uid), {
-    name: data.name,
-    note: data.note,
-    photo: data.photo || "",
-    createdAt: new Date()
-  });
-}
